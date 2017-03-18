81a0f55875736ad1b787a113bd29f1be8ee6eeeb
Adam Bryant <adam.w.bryant@outlook.com>
2017-03-18 12:40:13 -0400

    Preparation for repo seperation

---
+++ Engine.h
@@ -0,0 +1,10 @@
+#ifndef ENGINE_H
+
+#define ENGINE_H
+#include "classes/EventHandler.h"
+#include "classes/KeyboardHandler.h"
+#include "classes/Sprite.h"
+#include "classes/TextureManager.h"
+#include "classes/WindowManager.h"
+#include "classes/Player.h"
+#endif
